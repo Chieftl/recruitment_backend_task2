5634bc2aa519e92911d58e743a73e486c21f5c5d
Chieftl <ilkiv.yuriy@gmail.com>
2024-08-06 21:31:38 +0300

    Send form data to result page

--- src/App/Controller/TimezoneController.php
+++ src/App/Controller/TimezoneController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\TimezoneFormType;
+use App\Service\TimezoneService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,8 +18,24 @@ class TimezoneController extends AbstractController
     {
         $form = $this->createForm(TimezoneFormType::class);
 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+
+            return $this->redirectToRoute('app_timezone_result', $formData);
+        }
+
         return $this->render('timezone/index.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/timezone/result", name="app_timezone_result")
+     */
+    public function result(Request $request, TimezoneService $timezoneService): Response
+    {
+        $date = new \DateTime($request->get('date'));
+        $timezone = $request->get('timezone');
     }
 }
